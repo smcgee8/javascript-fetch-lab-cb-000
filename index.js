9d146efc1e579f4c5aaf1aaf16c416b9b0a76a57
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-02 23:59:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function forkRepo() {
   fetch('https://api.github.com/repos/' + repo + '/forks', {
     method: 'POST',
     headers: {
-    Authorization: `token ${getToken()}`
+      Authorization: `token ${getToken()}`
     }
   }).then(res => res.json()).then(showResults);
 }
