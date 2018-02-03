3b0c86bf3a4a64a609d5476e4ddbb854359c0622
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-03 02:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,5 +50,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return window.key;
+  return '';
 }
